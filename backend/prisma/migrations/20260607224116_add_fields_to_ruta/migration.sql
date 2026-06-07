531b63b69518e9cbfee60999d8561760b61d21f3
Tomas <tpenroz2024@alu.uct.cl>
2026-06-07 18:59:17 -0400

    se migro la logica de assignment al backend

---
+++ backend/prisma/migrations/20260607224116_add_fields_to_ruta/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Ruta" ADD COLUMN     "horaFin" TIMESTAMP(3),
+ADD COLUMN     "horaInicio" TIMESTAMP(3);
---
+++ backend/src/routes/assignmentRoutes.ts
@@ -0,0 +1,23 @@
+import { Router } from "express"
+import {
+  createAssignmentController,
+  startAssignmentController,
+  finishAssignmentController,
+  abortAssignmentController
+} from "../controller/controllerAssignment"
+
+const router = Router()
+
+// crear asignación
+router.post("/", createAssignmentController)
+
+// iniciar asignación
+router.post("/:id/start", startAssignmentController)
+
+// finalizar asignación
+router.post("/:id/finish", finishAssignmentController)
+
+// abortar asignación
+router.post("/:id/abort", abortAssignmentController)
+
+export default router
